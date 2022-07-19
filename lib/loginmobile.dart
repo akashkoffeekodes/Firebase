18fb73e72ff4dcbe7e5a83590c2b73fc0a249b5b
admin <akashkoffeekodes@gmail.com>
2022-07-19 18:16:37 +0530

    6:15    19/7/2022

--- lib/loginmobile.dart
+++ lib/loginmobile.dart
@@ -1,6 +1,11 @@
-import 'package:firebase/otp.dart';
+import 'package:alt_sms_autofill/alt_sms_autofill.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-//import 'package:phone_auth_project/otp.dart';
+import 'package:flutter/services.dart';
+import 'package:pinput/pin_put/pin_put.dart';
+import 'detail.dart';
+import 'mobilehome.dart';
+
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,10 +13,53 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _commingSms ;
   TextEditingController _controller = TextEditingController();
+
+  final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
+  String _verificationCode;
+  TextEditingController _pinPutController = TextEditingController();
+  final FocusNode _pinPutFocusNode = FocusNode();
+  final BoxDecoration pinPutDecoration = BoxDecoration(
+    color: Colors.tealAccent,
+    borderRadius: BorderRadius.circular(10.0),
+    border: Border.all(
+      color: const Color.fromRGBO(126, 203, 224, 1),
+    ),
+  );
+
+
+
+  Future<void> initSmsListener() async {
+    String commingSms;
+    try {
+      commingSms = await AltSmsAutofill().listenForSms;
+    } on PlatformException {
+      commingSms = 'Failed to get Sms.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _commingSms = commingSms;
+      if(_commingSms != null || _commingSms != "" ){
+        _controller.text = _commingSms.substring(0,6);
+      }
+
+    });
+  }
+
+
+  @override
+  void dispose() {
+    AltSmsAutofill().unregisterListener();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldkey,
       appBar: AppBar(
         title: Text('Phone Auth'),
       ),
@@ -24,7 +72,7 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Center(
                 child: Text(
                   'Phone Authentication',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
             ),
@@ -42,7 +90,53 @@ class _LoginScreenState extends State<LoginScreen> {
                 keyboardType: TextInputType.number,
                 controller: _controller,
               ),
-            )
+            ),
+            SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: PinPut(
+                fieldsCount: 6,
+                textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+                eachFieldWidth: 40.0,
+                eachFieldHeight: 55.0,
+                focusNode: _pinPutFocusNode,
+                controller: _pinPutController,
+                submittedFieldDecoration: pinPutDecoration,
+                selectedFieldDecoration: pinPutDecoration,
+                followingFieldDecoration: pinPutDecoration,
+                pinAnimationType: PinAnimationType.fade,
+                onSubmit: (pin) async {
+                  try {
+                    await FirebaseAuth.instance
+                        .signInWithCredential(PhoneAuthProvider.credential(
+                        verificationId: _verificationCode, smsCode: pin))
+                        .then((value) async {
+                      if (value.user != null) {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => Home()),
+                                (route) => false);
+                      }
+                    });
+                  } catch (e) {
+                    FocusScope.of(context).unfocus();
+                    _scaffoldkey.currentState
+                        .showSnackBar(SnackBar(content: Text('invalid OTP')));
+                  }
+                },
+              ),
+            ),
+
+            Center(
+              child: Text("$_commingSms"),
+
+            ),
+            TextButton(
+              child: Text('Listen for sms code'),
+              onPressed: initSmsListener,
+            ),
+
+
           ]),
           Container(
             margin: EdgeInsets.all(10),
@@ -50,8 +144,7 @@ class _LoginScreenState extends State<LoginScreen> {
             child: FlatButton(
               color: Colors.teal,
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => OTPScreen(_controller.text)));
+                _verifyPhone();
               },
               child: Text(
                 'Next',
@@ -62,5 +155,37 @@ class _LoginScreenState extends State<LoginScreen> {
         ],
       ),
     );
+  }
+  _verifyPhone() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: '+91 ${_controller.text}',
+
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await FirebaseAuth.instance
+              .signInWithCredential(credential)
+              .then((value) async {
+            if (value.user != null) {
+              print("Sucessfull Login");
+              // Navigator.pushAndRemoveUntil(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => AddDetail()),
+              //         (route) => false);
+            }
+          }).catchError((error) {});
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print(e.message);
+        },
+        codeSent: (String verficationID, int resendToken) {
+          setState(() {
+            _verificationCode = verficationID;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationID) {
+          setState(() {
+            _verificationCode = verificationID;
+          });
+        },
+        timeout: Duration(seconds: 60    ));
   }
 }
